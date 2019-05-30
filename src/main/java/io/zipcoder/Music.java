ad0dd80327a12a09a4e484cbd15704b1b08fb4e7
prasanthipenmadu <prasanthipl@gmail.com>
2019-05-30 13:44:37 -0400

    All Test Cases Passed

--- src/main/java/io/zipcoder/Music.java
+++ src/main/java/io/zipcoder/Music.java
@@ -3,12 +3,38 @@ package io.zipcoder;
 public class Music {
 
     private String[] playList;
+    int min1= 0;
+    int min2=0;
 
-    public Music(String[] playList){
+    public Music(String[] playList) {
         this.playList = playList;
     }
 
-    public Integer selection(Integer startIndex, String selection){
-        return null;
+    public Integer selection(Integer startIndex, String selection) {
+        int count=0;
+        int i=startIndex;
+
+            while (!playList[i].equalsIgnoreCase(selection)) {
+                count++;
+                i++;
+            }
+            min1 = count;
+            i=startIndex;
+            count=0;
+        while (!playList[i].equalsIgnoreCase(selection)) {
+            if(i==0){
+                i=playList.length;
+            }
+            count++;
+            i--;
+        }
+        min2 = count;
+
+        if(min1<min2)
+            return min1;
+        else
+            return min2;
+
     }
 }
+
